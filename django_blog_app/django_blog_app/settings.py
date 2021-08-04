0da596aca366050f8285581909ca1a8fe4cf605e
Riku <smile6280@icloud.com>
2021-08-04 12:22:07 +0900

    first commit

--- django_blog_app/django_blog_app/settings.py
+++ django_blog_app/django_blog_app/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+^u^7p@!*^d_ab#(*wsg)ydp@)1x4(9$!046(3fhn2giah4-z'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
