09d0ac80ffd28b09f9fdc7587b6ba4f89ce7521b
Riku <smile6280@icloud.com>
2021-08-07 19:13:50 +0900

    Add admin / models / migration

--- django_blog_app/blogs/models.py
+++ django_blog_app/blogs/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Blog(models.model):
-    title = models.CharField(blnak=False, null=False, max_length=150)
+class Blog(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=150)
     text = models.TextField(blank=True)
     created_detetime = models.DateTimeField(auto_now_add=True)
     updated_datetime = models.DateTimeField(auto_now=True)
