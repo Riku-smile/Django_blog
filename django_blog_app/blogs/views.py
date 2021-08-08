3ba52659eea0fc4cb4ce4c2eb8f1872b3d909a8f
Riku <smile6280@icloud.com>
2021-08-08 19:23:11 +0900

    Add blog_pages

--- django_blog_app/blogs/views.py
+++ django_blog_app/blogs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'blogs/index.html')
+    blogs = Blog.objects.order_by('-created_detetime')
+    return render(request, 'blogs/index.html', {'blogs': blogs})
+
+
+def detail(request, blog_id):
+    blog = Blog.objects.get(id=blog_id)
+    return render(request, 'blogs/detail.html', {'blog': blog})
